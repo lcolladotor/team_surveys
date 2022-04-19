0eaa665e15aab5eb39c0c95e884f011ccbef1bdf
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-04-19 14:49:25 -0400

    Show surveys in reverse order with the newest at the top
    Change single word character entries into barplots instead of showing the direct answers (aka, add more plots)
    
    Avoid code chunk name clash

--- csv_to_Rmd.R
+++ csv_to_Rmd.R
@@ -1,5 +1,8 @@
 library("glue")
+library("stringr")
 csvs <- dir(pattern = "\\.csv$", recursive = TRUE, full.names = TRUE)
+names(csvs) <- basename(dirname(csvs))
+csvs <- rev(csvs)
 
 answers <- lapply(csvs, read.csv, check.names = FALSE)
 
@@ -12,18 +15,41 @@ lapply(seq_along(answers), function(i) {
 
         x <- answers[[i]][[j]]
         question <- colnames(answers[[i]])[j]
+        year <- names(csvs)[i]
 
         if(is.character(x)) {
             ## Remove any redacted text
             x <- gsub("<redact>.*<\\/redact>", "", x)
             x <- sample(x)
-            res <- paste(glue("* {x}"), collapse = "\n")
+            if(all(str_count(x, "\\w+") == 1)) {
+                ## Cases where there are single word answers
+                x <- paste0('"', paste0(x, collapse = '", "'), '"')
+                res <- glue('
+```{r "plot_<j>_<year>", echo = FALSE, message = FALSE}
+library("ggplot2")
+library("dplyr")
+data.frame(x = c(<x>)) %>%
+    count(x) %>%
+    ggplot(aes(x = x, y = n)) + geom_bar(stat = "identity") +
+    theme_bw(base_size = 20) + xlab("") + ylab("count")
+```
+
+', .open = "<", .close = ">")
+
+                data.frame(x = x) %>% count(x) %>%
+                    mutate(percent = round(n / sum(n) * 100, 2)) %>%
+                    ggplot(aes(x = x, y = percent)) + geom_bar(stat = "identity") +
+                    theme_bw(base_size = 20) + xlab(question)
+
+            } else {
+                res <- paste(glue("* {x}"), collapse = "\n")
+            }
         } else {
             info <- paste0("1:", gsub(".*\\(1:|\\)", "", question))
 
         x <- toString(x)
         res <- glue('
-```{r "plot_<j>", echo = FALSE}
+```{r "plot_<j>_<year>", echo = FALSE}
 library("ggplot2")
 ggplot(data.frame(x = c(<x>)), aes(x = x)) + geom_bar() + theme_bw(base_size = 20) + xlab("<info>") + scale_x_continuous(breaks = 1:10, limits = c(0, 10.9))
 ```
@@ -45,5 +71,5 @@ ggplot(data.frame(x = c(<x>)), aes(x = x)) + geom_bar() + theme_bw(base_size = 2
     chapter_rmd <- glue("# Survey {date}
 
 {entries_combined}")
-    writeLines(chapter_rmd, paste0("survey_", date, ".Rmd"))
+    writeLines(chapter_rmd, paste0(sprintf("%02d", i), "_survey_", date, ".Rmd"))
 })
